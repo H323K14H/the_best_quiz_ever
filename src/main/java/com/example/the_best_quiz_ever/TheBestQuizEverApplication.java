bf7c63e4ce890c6c6315db7498905aad5bca145c
Hezekiah Adeusi <hezekiaha58@gmail.com>
2023-11-10 16:08:42 +0000

    Initial commit:
    - Project skeleton created via Spring initializr.

---
+++ src/main/java/com/example/the_best_quiz_ever/TheBestQuizEverApplication.java
@@ -0,0 +1,13 @@
+package com.example.the_best_quiz_ever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBestQuizEverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBestQuizEverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the_best_quiz_ever/TheBestQuizEverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the_best_quiz_ever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBestQuizEverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
