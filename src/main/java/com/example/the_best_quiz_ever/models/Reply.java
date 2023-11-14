e22e7035497dad8c0133071330b0e94c834efd46
Rosie Miall <rosiemiall38@gmail.com>
2023-11-14 10:45:33 +0000

    change of variable name in reply class

--- src/main/java/com/example/the_best_quiz_ever/models/Reply.java
+++ src/main/java/com/example/the_best_quiz_ever/models/Reply.java
@@ -2,14 +2,14 @@ package com.example.the_best_quiz_ever.models;
 
 public class Reply {
 
-    private String selectQuestion;
+    private String currentQuestion;
 
     private String options;
 
     private boolean optionSelected;
 
     public Reply(String selectQuestion, String options, boolean optionSelected) {
-        this.selectQuestion = selectQuestion;
+        this.currentQuestion = selectQuestion;
         this.options = options;
         this.optionSelected = optionSelected;
     }
@@ -19,11 +19,11 @@ public class Reply {
     }
 
     public String getSelectQuestion() {
-        return selectQuestion;
+        return currentQuestion;
     }
 
     public void setSelectQuestion(String selectQuestion) {
-        this.selectQuestion = selectQuestion;
+        this.currentQuestion = selectQuestion;
     }
 
     public String getOptions() {
