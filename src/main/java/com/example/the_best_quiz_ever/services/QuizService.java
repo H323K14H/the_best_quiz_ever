65d793a832712fbc6e7d17af4a1397f7ff68470d
Rosie Miall <rosiemiall38@gmail.com>
2023-11-14 10:53:32 +0000

    property added to quizService

--- src/main/java/com/example/the_best_quiz_ever/services/QuizService.java
+++ src/main/java/com/example/the_best_quiz_ever/services/QuizService.java
@@ -4,9 +4,14 @@ import com.example.the_best_quiz_ever.repositories.QuizRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class QuizService {
     @Autowired
     QuizRepository quizRepository;
+
+    private ArrayList<String> selectedOptions;
+    
     
 }
